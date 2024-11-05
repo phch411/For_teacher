5e32baa0b4e934393f09179598bc9919c495bf49
phch411 <phch411@gmail.com>
2024-11-05 22:24:11 +0900

    입력창 사이드바로 옮기기

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,30 +16,30 @@ with st.sidebar:
     name = st.text_input('이름을 입력하세요', '')
 
 
-# 학년 옵션 리스트 생성
-grades = [
-    '1학년', '2학년', '3학년', '4학년', '5학년', '6학년'
-]
+    # 학년 옵션 리스트 생성
+    grades = [
+        '1학년', '2학년', '3학년', '4학년', '5학년', '6학년'
+    ]
 
-# 선택창 생성
-selected_grade = st.selectbox('학년을 선택하세요:', grades)
+    # 선택창 생성
+    selected_grade = st.selectbox('학년을 선택하세요:', grades)
 
-# 반 옵션 리스트 생성
-classes = [
-    '1반', '2반'
-]
+    # 반 옵션 리스트 생성
+    classes = [
+        '1반', '2반'
+    ]
 
-# 반 선택창 생성
-selected_class = st.selectbox('반을 선택하세요:', classes)
+    # 반 선택창 생성
+    selected_class = st.selectbox('반을 선택하세요:', classes)
 
-# 번호 입력창 생성
-number = st.number_input('번호를 입력하세요', min_value=1, step=1, value=1)
+    # 번호 입력창 생성
+    number = st.number_input('번호를 입력하세요', min_value=1, step=1, value=1)
 
-# 시작날짜 선택창 생성
-start_date = st.date_input("시작날짜를 선택하세요", date.today())
+    # 시작날짜 선택창 생성
+    start_date = st.date_input("시작날짜를 선택하세요", date.today())
 
-# 끝 날짜 선택창 생성
-end_date = st.date_input("끝나는 날짜를 선택하세요", date.today())
+    # 끝 날짜 선택창 생성
+    end_date = st.date_input("끝나는 날짜를 선택하세요", date.today())
 
 # 기간 알리는 창 만들기
 if start_date <= end_date:
@@ -53,14 +53,14 @@ if start_date <= end_date:
 else:
     st.error("종료 날짜는 시작 날짜보다 늦어야 합니다.")
 
-# 세부사항 옵션 리스트 생성
-details = [
-    '출석인정', '질병', '기타', '미인정'
-]
+    # 세부사항 옵션 리스트 생성
+    details = [
+        '출석인정', '질병', '기타', '미인정'
+    ]
 
-# 선택창 생성
-selected_details = st.selectbox('결석 세부사항을 선택하세요:', details)
+    # 선택창 생성
+    selected_details = st.selectbox('결석 세부사항을 선택하세요:', details)
 
-# 사유 입력창 생성
-reason = st.text_input('사유를 입력하세요', '')
+    # 사유 입력창 생성
+    reason = st.text_input('사유를 입력하세요', '')
 
